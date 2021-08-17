49d4744bf59e54f5198915c0cebc79e7578b5d92
Gabriel Mendes Martins - GMR <gabriel.martins@iteris.local>
2021-08-16 22:44:38 -0300

    first commit

---
+++ openOutlookAndBrowser.ps1
@@ -0,0 +1,10 @@
+﻿##Inicia Outlook no Windows StarTup
+$outlook = new-object -com Outlook.Application
+$namespace = $outlook.GetNamespace("MAPI")
+$folder = $namespace.GetDefaultFolder([Microsoft.Office.Interop.Outlook.OlDefaultFolders]::olFolderInbox)
+$explorer = $folder.GetExplorer()
+$explorer.Display()
+
+##Inicia navegador padrão com a URL especificada
+$urlSitePadrao="https://www.google.com"
+Start-Process $urlSitePadrao
